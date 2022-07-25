f86259ed5a678c10282eef8f6435f4ddd4f73f17
sayfutdiinov <sayfutdinovsaman@gmail.com>
2022-07-25 21:57:59 +0500

    First changes

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var elBurrgerBtn=document.querySelector("header-box__btn");
+var elHrNavmb=document.querySelector("st-naw");
+
+
+elBurrgerBtn.addEventListener('click', () =>{
+    elHrNavmb.classList.toggle('active')
+})
